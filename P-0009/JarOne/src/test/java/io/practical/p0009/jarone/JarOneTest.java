7bcc3f1cb3fddcbffae6496944ac1ae8bac240db
Aurore SIMON <simon_aurore#hotmail.com>
2017-11-27 21:11:33 +0100

    Add P-0009

---
+++ P-0009/JarOne/src/test/java/io/practical/p0009/jarone/JarOneTest.java
@@ -0,0 +1,13 @@
+package io.practical.p0009.jarone;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JarOneTest {
+
+	@Test
+	public void test() {
+		Assertions.assertTrue(true);
+	}
+
+}
